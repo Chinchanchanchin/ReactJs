3cd912f23a754c35396f11745b6bdc6f39a12076
Chinchanchanchin <chanchin2102@gmail.com>
2022-04-07 22:50:25 +0900

    them

---
+++ ES6/main.js
@@ -0,0 +1,17 @@
+let greet = "Xin chao CODEGYM ONLINE!";
+const LESSON = "Su dung Module trong ES6";
+
+function greeting(name, content){
+    return name + " " + content;
+}
+
+export { greet, LESSON, greeting };
+// let greet = "Xin chào CODEGYM ONLINE!";
+// const LESSON = "Sử dụng Module trong ES6";
+ 
+// function greeting(name, content) {
+//     return name + " " +content;
+// }
+ 
+// // Xuất các biến và hàm
+// export { greet, LESSON, greeting };
